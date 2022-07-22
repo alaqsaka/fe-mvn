47841ca69f40fdc55ac019fc7aba3bad9dbd70d4
alaqsaka <aqsaabidin15@gmail.com>
2022-07-22 20:09:19 +0700

    add background color to pecahancard

--- components/PecahanCard.tsx
+++ components/PecahanCard.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PecahanCard = ({digit}: {digit: number}) => {
     // Terima props berupa angka
   return (
-    <div className='border-2 border-gray-400 p-4 rounded-md mt-2 mb-2'>{digit}</div>
+    <div className='border-2 border-gray-400 p-4 rounded-md mt-2 mb-2 bg-slate-200'>{digit}</div>
   )
 }
 
